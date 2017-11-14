3437a16397875d9600532b73da73eb66d936e1db
Matthew Mosesohn <matthew.mosesohn@gmail.com>
2017-11-14 18:06:32 +0000

    workaround httpd package install cpio error

--- image/scripts/install_system.sh
+++ image/scripts/install_system.sh
@@ -3,6 +3,8 @@
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 yum -y install http://yum.opennms.org/repofiles/opennms-repo-stable-rhel7.noarch.rpm
+# ignore cpio error on ubuntu build host
+yum -y install httpd.x86_64 || true
 yum -y install wget.x86_64 httpd.x86_64 mod_php56w.x86_64 httpd-devel.x86_64 httpd-itk.x86_64 httpd-manual.noarch httpd-tools.x86_64 mod_ssl.x86_64 php56w.x86_64 php56w-opcache.x86_64 php56w-mysqlnd.x86_64 php56w-gd.x86_64 php56w-pecl-apcu \
   php56w-posix php56w-mcrypt.x86_64 php56w-pear.noarch cronie.x86_64 net-snmp.x86_64 yum-cron.noarch supervisor.noarch \
   net-snmp-utils.x86_64 fping.x86_64 MySQL-python.x86_64 libvirt.x86_64 \
